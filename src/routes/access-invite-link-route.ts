92e4f5dc655f6b9867cc6b04fcc3d10c21b913f0
Camila Pinheiro <camilampinheiro1108@gmail.com>
2025-02-21 00:10:58 -0300

    commit final da aula 3

--- src/routes/access-invite-link-route.ts
+++ src/routes/access-invite-link-route.ts
@@ -2,22 +2,19 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 import { env } from '../env'
 import { accessInviteLink } from '../functions/access-invite-link'
-import { redis } from '../redis/client'
 
 export const accessInviteLinkRoute: FastifyPluginAsyncZod = async app => {
   app.get(
     '/invites/:subscriberId',
     {
       schema: {
-        summary: 'Access invite link and redirects user ',
+        summary: 'Access invite link',
         tags: ['referral'],
         params: z.object({
           subscriberId: z.string(),
         }),
         response: {
-          201: z.object({
-            subscriberId: z.string(),
-          }),
+          301: z.null(),
         },
       },
     },
